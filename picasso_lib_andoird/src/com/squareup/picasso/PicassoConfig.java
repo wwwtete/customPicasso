6b8bbbdfb2613f744f4849330b00e436ff0463b5
wangw <wwwtete@163.com>
2014-09-24 10:52:33 +0800

    初始化提交

---
+++ picasso_lib_andoird/src/com/squareup/picasso/PicassoConfig.java
@@ -0,0 +1,21 @@
+package com.squareup.picasso;
+
+import android.text.TextUtils;
+
+/**
+ * @author wangw
+ * Picasso配置文件
+ */
+public class PicassoConfig {
+
+	
+	/**
+	 * 图片缓存路径
+	 */
+	public static String CACHEDIR = "";
+	/**缓存大小*/
+	public static int CACHE_MAXSIZE = -1;
+	
+	
+	
+}
